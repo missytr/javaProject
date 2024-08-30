e059388050a86114078f5f31ca2036d33588670c
missytr <134213107+missytr@users.noreply.github.com>
2024-08-30 21:42:35 +0700

    Create moneyExchange.java

---
+++ moneyExchange.java
@@ -0,0 +1,14 @@
+package PACKAGE_NAME;
+
+import java.util.Scanner;
+public class moneyExchange {
+    public static void main(String[] args) {
+        double USD;
+        double exchangeRate = 23.500;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Đổi tiền USD => VND");
+        System.out.print("Vui lòng nhập số USD cần đổi: ");
+        USD = sc.nextDouble();
+        System.out.println("Số VND sau khi đổi là: " + (USD * exchangeRate) + " VND" );
+    }
+}
