4274d7ed598fa9237caa3657f39b448ae98e2c01
missytr <134213107+missytr@users.noreply.github.com>
2024-08-30 21:08:26 +0700

    Update Average.java

--- Average.java
+++ Average.java
@@ -1,4 +1,21 @@
 package PACKAGE_NAME;
 
+import java.util.Scanner;
 public class Average {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a,b,c,d,e;
+        System.out.println("Tính trung bình năm số cần nhập");
+        System.out.print("Vui lòng nhập số thứ nhất: ");
+        a = sc.nextInt();
+        System.out.print("Vui lòng nhập số thứ hai: ");
+        b = sc.nextInt();
+        System.out.print("Vui lòng nhập số thứ ba: ");
+        c = sc.nextInt();
+        System.out.print("Vui lòng nhập số thứ bốn: ");
+        d = sc.nextInt();
+        System.out.print("Vui lòng nhập số thứ năm: ");
+        e = sc.nextInt();
+        System.out.println("Trung bình của năm số nhập là: " + (float)(a+b+c+d+e)/5);
+    }
 }
